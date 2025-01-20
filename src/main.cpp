ad5ee4d35d38a2e4cb418b058d1ce091157ebbc8
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-01-19 17:10:53 -0800

    Added layer system
    Very cool

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 #include <raylib.h>
 
+enum Layer {
+    MainMenu,
+    Playing
+};
+
 int main() {
     InitWindow(1920, 1080, "RedBox");
 
     SetTargetFPS(60);
 
+    // Variables for the game to work
+    Layer current_layer = MainMenu;
+
     while (!WindowShouldClose()) {
+        bool is_enter_pressed = IsKeyPressed(KEY_ENTER);
+
         BeginDrawing();
 
-        ClearBackground(BLACK);
+        if (current_layer == MainMenu) {
+            ClearBackground(BLACK);
 
-        DrawText("no game yet", 960, 540, 30, WHITE);
+            DrawText("Press enter to start!", 960, 540, 30, WHITE);
+
+            if (is_enter_pressed) {
+                current_layer = Playing;
+            }
+        } else if (current_layer == Playing) {
+            ClearBackground(BLACK);
+
+            DrawText("no game yet", 960, 540, 30, WHITE);
+        }
 
         EndDrawing();
     }
